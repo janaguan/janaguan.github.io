63ebcf55f9370fe7762d46b4fec555efe86b2571
janaguan <jana.guan@yahoo.com>
2016-11-21 19:01:13 -0800

    DONE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+$('#sidebar-button').click(function() {
+  if ($("#sidebar-button").hasClass("button-active")) {
+    $('body').removeClass('no-scroll');
+    $('#sidebar-button').removeClass('button-active');
+    $('.sidebar-container').removeClass('sidebar-active');
+    $('.page-wrapper').removeClass('wrapper-active');
+  } else {
+      $('#sidebar-button').addClass('button-active');
+      $('.sidebar-container').addClass('sidebar-active');
+      $('.page-wrapper').addClass('wrapper-active');
+      $('body').addClass('no-scroll');
+      setTimeout(function() {
+        $('body').addClass('no-scroll');
+        }, 300);
+    }
+});
+
+$('.page-wrapper').click(function(){
+  if ($("#sidebar-button").hasClass("button-active")) {
+    $('body').removeClass('no-scroll');
+    $('#sidebar-button').removeClass('button-active');
+    $('.sidebar-container').removeClass('sidebar-active');
+    $('.page-wrapper').removeClass('wrapper-active');
+  }
+});
+
 
 
 });
