d89c09f37d4c8aa3935074928600349cf2d798e6
janaguan <jana.guan@yahoo.com>
2016-11-26 13:37:32 -0800

    Lots of Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
-$(document).ready(function() {
-
-  
-
-/*
+$(document).ready(function(){
+  //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
 $('#sidebar-button').click(function() {
   if ($("#sidebar-button").hasClass("button-active")) {
     $('body').removeClass('no-scroll');
@@ -20,6 +17,9 @@ $('#sidebar-button').click(function() {
     }
 });
 
+  // QUESTION 7
+
+  //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
 $('.page-wrapper').click(function(){
   if ($("#sidebar-button").hasClass("button-active")) {
     $('body').removeClass('no-scroll');
@@ -28,7 +28,47 @@ $('.page-wrapper').click(function(){
     $('.page-wrapper').removeClass('wrapper-active');
   }
 });
-*/
+
+$(".bakery").click(function() {
+    $("#overlay").show();
+    $(this).addClass("bakery-pic-click");
+    $(this).addClass("height-width");
+    $('body').addClass('no-scroll');
+  });
+
+$("#overlay").click(function() {
+    $(this).hide();
+    $(".bakery").removeClass("bakery-pic-click");
+    $(".bakery").removeClass("height-width");
+    $('body').removeClass('no-scroll');
+  });
+
+$("#season-img").click(function() {
+    $("#overlay").show();
+    $(this).addClass("season-pic-click");
+    $('body').addClass('no-scroll');
+    $("#season-img").css("max-height", "1100px");
+    $("#season-img").css("max-width", "1100px");
+  });
+
+$("#overlay").click(function() {
+    $(this).hide();
+    $("#season-img").removeClass("season-pic-click");
+    $('body').removeClass('no-scroll');
+    $("#season-img").css("max-width", "800px")
+    $("#season-img").css("max-height", "750px")
+  });
+
+$("a").click(function(){
+  $('body').removeClass('no-scroll');
+  $("html, body").animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 400);
+  $(this).delay(5000);
+  $('#sidebar-button').removeClass('button-active');
+  $('.sidebar-container').removeClass('sidebar-active');
+  $('.page-wrapper').removeClass('wrapper-active');
+});
 
 
 });
